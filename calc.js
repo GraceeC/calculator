15accb46f32f4ce778155513a8c1d7f76ea8d6a7
GraceeC <51942103+GraceeC@users.noreply.github.com>
2019-07-06 08:15:44 -0700

    Update calc.js

--- calc.js
+++ calc.js
@@ -1,8 +1,15 @@
 function dis(value)  { 
-    document.getElementById("input-text")=value
+    document.getElementById("input-text").value+=value 
 } 
-   function solve() 
-{ 
+
+
+function solve() { 
     let x = document.getElementById("input-text").value 
     let y = eval(x) 
     document.getElementById("input-text").value = y 
+} 
+
+
+    document.getElementById("seven").addEventListener("click", function() {
+        console.log(7);
+    });
