6fea064c968fbc1f593086e613b399f7b300f5b2
GraceeC <51942103+GraceeC@users.noreply.github.com>
2019-07-08 08:34:42 -0700

    Update calc.js

--- calc.js
+++ calc.js
@@ -1,15 +1,42 @@
+let operator1; //undefined
+let operator2; //undefined
+
+
 function dis(value)  { 
-    document.getElementById("input-text").value+=value 
+    document.getElementById("input-text").value = value; 
+
+if (operator1) {
+    operator2 = value;
+}else {
+    operator1 = value;
+}
+
+
+console.log('operator1:', operator1);
+console.log('operator2:', operator2);
+    
 } 
+function add() {
+    console.log("add button was clicked!");
+}
+function sub() {
+    console.log("subtract button was clicked");
+}
+function multi() {
+    console.log("multiplication button was clicked");
+}
 
 
-function solve() { 
-    let x = document.getElementById("input-text").value 
-    let y = eval(x) 
-    document.getElementById("input-text").value = y 
-} 
+function equals() {
+    document.getElementById("input-text").value = parseInt(operator1, 10) + parseInt(operator2, 10);
+}
+
+function equals() {
+    document.getElementById("input-text").value = parseInt(operator1, 10) - parseInt(operator2, 10);
+}
+
+function equals() {
+    document.getElementById("input-text").value = parseInt(operator1, 10) * parseInt(operator2, 10);
+}
 
 
-    document.getElementById("seven").addEventListener("click", function() {
-        console.log(7);
-    });
