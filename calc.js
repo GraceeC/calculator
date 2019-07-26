84564ad99dec01ef5c289de79e92c8a3225ed37b
GraceeC <51942103+GraceeC@users.noreply.github.com>
2019-07-25 22:21:10 -0700

    Update calc.js

--- calc.js
+++ calc.js
@@ -2,19 +2,25 @@
 let operation; //undefined 
 let operator1; //undefined
 let operator2; //undefined
+//let memory = ['MPLUS', 'MMINUS', 'MR'];
 
 //value
 function dis(value) {
     document.getElementById("input-text").value = value;
 
-    if (operator1){
+    if (operator1) {
         operator2 = value;
-    }else {
+    } else {
         operator1 = value;
-    
-    console.log('operator1:', operator1);
-    console.log('operator2:', operator2);  
+
+        console.log('operator1:', operator1);
+        console.log('operator2:', operator2);
     }
+}
+
+function length() {
+    if (operator1 += operator2.length > 10);
+        return operator(0, operator.length);
 }
 
 //operators 
@@ -22,52 +28,48 @@ function add() {
     operation = 'ADD';
     console.log("you clicked the add button");
 }
+
 function sub() {
     operation = 'SUBTRACT';
     console.log("you clicked the subtract button");
 }
+
 function divide() {
     operation = 'DIVIDE';
     console.log("you clicked the divide button");
 }
+
 function multi() {
     operation = 'MULTIPLY';
     console.log("you clicked the mutiply button");
-}    
-//Length
-function OperatorLength(operator){
-    if (operator1 && operator2.length > 10) {    
-        return operator.substring(0,operator.length);
-    } else {
-        return operator;
-    }
 }
 
 //Clear 
 function reset() {
-    document.getElementById("input-text").value=0;
-    operator='undefined';
-    opertaor1='undefined';
-    operator2='undefined';
+    document.getElementById("input-text").value = 0;
+    operator = 'undefined';
+    opertaor1 = 'undefined';
+    operator2 = 'undefined';
     console.log('clear');
 }
-function MPLUS(){
-    document.getElementById("input-text").value=" ";
-    operator='undefined';
-    opertaor1='undefined';
-    operator2='undefined';
-    console.log('MPLUS');
-}
+
+//function MPLUS(){
+//    document.getElementById("input-text").value=" ";
+//    operator='undefined';
+//    opertaor1='undefined';
+//    operator2='undefined';
+//    console.log('MPLUS');
+//}
 
 //function math operoters
 function equals() {
     if (operation === 'ADD') {
         document.getElementById("input-text").value = parseInt(operator1, 10) + parseInt(operator2, 10);
-    } else if(operation === 'SUBTRACT') {
+    } else if (operation === 'SUBTRACT') {
         document.getElementById("input-text").value = parseInt(operator1, 10) - parseInt(operator2, 10);
-    } else if(operation === 'DIVIDE') {
+    } else if (operation === 'DIVIDE') {
         document.getElementById("input-text").value = parseInt(operator1, 10) / parseInt(operator2, 10);
-    } else if(operation === 'MULTIPLY') {
+    } else if (operation === 'MULTIPLY') {
         document.getElementById("input-text").value = parseInt(operator1, 10) * parseInt(operator2, 10);
     }
 }
